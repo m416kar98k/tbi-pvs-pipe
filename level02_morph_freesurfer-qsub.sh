bb4f891b092e25cfd25bdc06bba3bfa1348c3533
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 20:35:02 -0700

    Update level02_morph_freesurfer-qsub.sh

--- level02_morph_freesurfer-qsub.sh
+++ level02_morph_freesurfer-qsub.sh
@@ -1,3 +1,15 @@
+#############################################################
+# Level 2 roi analysis 
+# scripts includes:
+# 	- run freesurfer on every subject 
+
+# Zhuocheng Li - zhuocheng.li@loni.usc.edu
+# INI Microstructural imaging Group (IMG)
+# Quantitative Imaging Team (QI-Team)
+# Steven Neuroimaging and Informatic Institute 
+# Keck school of medicine of USC
+#############################################################
+
 # recall var1 is the analysis absolute path
 cd ${1}
 for i in *
