58a80011fd969e7a5f357d7b779ae6d71ba0fa52
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 22:54:25 -0700

    Update level04_ants-seg-thickness.sh

--- level04_ants-seg-thickness.sh
+++ level04_ants-seg-thickness.sh
@@ -17,7 +17,7 @@ atlasF="/usr/local/NeuroBattery/data/template/PTBP"
 for i in *
 do
 	mkdir -p ${i}/ants
-	qsubcmd sh ${ANTSPATH}/antsCorticalThickness.sh -d 3 \
+	qsubcmd sh antsCorticalThickness.sh -d 3 \
 		-a ${PWD}/${s}/smri/t1w.nii.gz  \
 		-e ${atlas_folder}/PTBP_T1_Defaced.nii.gz \
 		-m ${atlas_folder}/PTBP_T1_BrainCerebellumProbabilityMask.nii.gz \
