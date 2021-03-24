2b73a4443f081f910afbaf40ed941fe9d17bbc80
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 22:42:21 -0700

    Update level01-preproc-standardize.sh

--- level01-preproc-standardize.sh
+++ level01-preproc-standardize.sh
@@ -3,9 +3,8 @@
 # scripts includes:
 # 	- volume standardize 
 
-# Zhuocheng Li - zhuocheng.li@loni.usc.edu
-# INI Microstructural imaging Group (IMG)
 # Quantitative Imaging Team (QI-Team)
+# INI Microstructural imaging Group (IMG)
 # Steven Neuroimaging and Informatic Institute 
 # Keck school of medicine of USC
 #############################################################
