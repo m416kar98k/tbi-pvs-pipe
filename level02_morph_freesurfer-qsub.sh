aaa0c837cf75415df93d7419aa01469563f41bcf
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 19:34:59 -0700

    Update level02_morph_freesurfer-qsub.sh

--- level02_morph_freesurfer-qsub.sh
+++ level02_morph_freesurfer-qsub.sh
@@ -1,8 +1,8 @@
 # recall var1 is the analysis absolute path
-cd ${var1}
+cd ${1}
 for i in *
 do
 	if [ ! -f "${data_folder}/${subName}/fsout/stats/aseg.stats" ]; then
-  		qsubcmd bash ./level02_morph_freesurfer-subject.sh ${i}
+  		qsubcmd bash ./level02_morph_freesurfer-subject.sh ${1} ${2}
 	fi
 done
