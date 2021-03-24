c2d0a86cd0ccf3a93643111c6eb863dee00aa069
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 23:10:15 -0700

    Update level04_jstabl-qsub.sh

--- level04_jstabl-qsub.sh
+++ level04_jstabl-qsub.sh
@@ -2,7 +2,7 @@ cd ${1}
 
 for i in *
 do
-  if [ -d "$i/fsout" ] && [ -f "$i/smri/flair.nii.gz" ]; then
+  if [ -d "$i/fsout" ] && [ -f "$i/smri/flair.nii.gz" ] && [ -f "$i/jstabl/skullstripping/T1_mask.nii.gz" ]; then
     qsubcmd bash ./jstabl_subject.sh $1 $i
   fi
 done
