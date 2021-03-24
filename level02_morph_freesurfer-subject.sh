9878b77ea9d7f985b24809734e15724c518a5b4f
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 19:52:14 -0700

    Create level02_morph_freesurfer-subject.sh

---
+++ level02_morph_freesurfer-subject.sh
@@ -0,0 +1,12 @@
+cd ${1}
+unset SUBJECTS_DIR
+SUBJECTS_DIR="${2}/smri"
+export SUBJECTS_DIR
+# define t1 image
+t1="${data_folder}/smri/t1w.nii.gz"
+## freesurfer bit + modifying the output
+recon-all -s ${2} -i ${t1} -all
+mkdir -p ${2}/fsout/
+mv ${2}/smri/${2}/* ${2}/fsout/
+# convert to nifti
+mri_convert ${2}/fsout/mri/brainmask.mgz ${2}/smri/t1w.brain.nii.gz
