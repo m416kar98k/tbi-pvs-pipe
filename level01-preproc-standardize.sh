75ed3d4ecb18b484c2168fb90bf11a493934cf0b
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 19:14:39 -0700

    Update level01-preproc-standardize.sh

--- level01-preproc-standardize.sh
+++ level01-preproc-standardize.sh
@@ -4,12 +4,12 @@ do
 	if [ ! -f $i/smri/t1w.orig.nii.gz ];then
     # backup the scans
 		qsubcmd cp ${i}/smri/t1w.nii.gz ${i}/smri/t1w.orig.nii.gz
-    qsubcmd cp ${i}/smri/t2w.nii.gz ${i}/smri/t2w.orig.nii.gz
+    		qsubcmd cp ${i}/smri/t2w.nii.gz ${i}/smri/t2w.orig.nii.gz
 		qsubcmd cp ${i}/smri/flair.nii.gz ${i}/smri/flair.orig.nii.gz
 		qsubcmd cp ${i}/dmri/dwi.nii.gz ${i}/dmri/dwi.orig.nii.gz
     # standardize the scans
 		qsubcmd qit VolumeStandardize --input ${i}/smri/t1w.orig.nii.gz --output ${i}/smri/t1w.nii.gz
-    qsubcmd qit VolumeStandardize --input ${i}/smri/t2w.orig.nii.gz --output ${i}/smri/t2w.nii.gz
+    		qsubcmd qit VolumeStandardize --input ${i}/smri/t2w.orig.nii.gz --output ${i}/smri/t2w.nii.gz
 		qsubcmd qit VolumeStandardize --input ${i}/smri/flair.orig.nii.gz --output ${i}/smri/flair.nii.gz
 		qsubcmd qit VolumeStandardize --input ${i}/dmri/dwi.orig.nii.gz --output ${i}/dmri/dwi.nii.gz
 	fi
