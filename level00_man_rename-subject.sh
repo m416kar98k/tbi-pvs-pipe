52f6348e783f1fcda21c284b63f0a4d23b3da883
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 19:37:21 -0700

    Update level00_man_rename-subject.sh

--- level00_man_rename-subject.sh
+++ level00_man_rename-subject.sh
@@ -1,20 +1,20 @@
-# recall var1 is the zip absolute path
-# recall var2 is the formatted subject name
-# recall var3 is the modality
-# recall var4 is the modality type
-# recall var5 is the analysis absolute path
+# recall var1 is the analysis absolute path
+# recall var2 is the zip absolute path
+# recall var3 is the formatted subject name
+# recall var4 is the modality
+# recall var5 is the modality type
 
 ## enter the analysis
-cd ${5}
-# make a directory for the subject
-mkdir ./${2}
+cd ${1}
+# create a directory for the subject
+mkdir ./${3}
 # enter the subject
-cd ${2}
+cd ${3}
 # make a raw directory for storing decompressing dicom files
 mkdir ./raw
 # make an output directory for storing the converted modality
-mkdir ./${4}
-unzip -q ${1} -d ./raw
-dcm2niix -z y -b y -x n -t n -m n -f ${3} -o ./${4} -s n -v n ./raw
-## clean the cache
+mkdir ./${5}
+unzip -q ${2} -d ./raw
+dcm2niix -z y -b y -x n -t n -m n -f ${4} -o ./${5} -s n -v n ./raw
+## cache
 rm -rf ./raw
