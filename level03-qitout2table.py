8a20c7a54ce59fd431639d67922342b0cdcd07f7
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-03-23 22:01:51 -0700

    Update level03-qitout2table.py

--- level03-qitout2table.py
+++ level03-qitout2table.py
@@ -21,10 +21,12 @@ df2 = []
 
 subj_list = os.listdir(analysisF)
 for i in subj_list:
+	column_names = []
 	csv_path = i + "/" + "qitout" + "/" + "diff.regions" + "/" + "jhu.labels.dti.map"
 	if os.path.isdir(csv_path):
+		column_names += [i.replace(".csv", "") + "_" + k for k in temp["name"]]
 		for j in os.listdir(csv_path):
 			temp = pd.read_csv(csv_path + "/" + j)
 			df2.append([i] + list(temp["value"]))
 
-pd.DataFrame(df2, columns = ["subject"] + [i.replace(".csv", "") + "_" + i for i in temp["name"]]).to_csv(statF + "/" + "qitout.csv")
+pd.DataFrame(df2, columns = ["Subject"] + column_names).to_csv(statF + "/" + "qitout.csv")
