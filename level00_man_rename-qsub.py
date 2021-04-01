68b8b18bf169f18ab12aba09f943c78b879c088b
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-04-01 16:51:27 -0700

    Update level00_man_rename-qsub.py

--- level00_man_rename-qsub.py
+++ level00_man_rename-qsub.py
@@ -15,21 +15,24 @@ import pandas as pd
 # cohort name
 cohort_name = input("Cohort name: ")
 modality = input("Modality name: ")
+
 if modality in ("t1w", "t2w", "flair"):
 	modality_type =  "smri"
 elif modality in ("dwi"):
 	modality_type = "dmri"
 
+root_path = input("Root directory: ")
+
 # demographic tables
-csv_path = input("Demographic table directory: ")
+csv_path = root_path + "/csv"
 csv_list = os.listdir(csv_path)
 
 # raw compressed dicom files
-zip_path = input("Raw scan directory: ")
+zip_path = root_path + "/zip"
 zip_list = os.listdir(zip_path)
 
 # output
-analysisF = input("Analysis directory: ")
+analysisF = root_path + "/analysis"
 
 # create a dictionary for converting image filename to subjectID and date/visit
 subj_list = []
@@ -43,13 +46,16 @@ for i in csv_list:
 	df = pd.read_csv(cur_path, header = 1)
 	subj_list += list(df["Main.GUID"])
 	date_list += list(df["Main.VisitDate"])
-	imag_list += list(df["Image Information.ImgFile"]])
+	imag_list += list(df["Image Information.ImgFile"])
 
-imag_list = list(map(lambda x: x.split("/")[-1], imag_list))
+subj_list = list(map(lambda x: x.replace("TBI_",""), subj_list))
+
+date_list = list(map(lambda x: x.replace("-",""), date_list))
+imag_list = list(map(lambda x: x.split("\\")[-1], imag_list))
 subj_date_list = [cohort_name + "_" + i + "_" + j if isinstance(i, str) and isinstance(j, str) else None for (i,j) in zip(subj_list, date_list)]
 for i in range(len(subj_date_list)):
 	if isinstance(subj_date_list[i], str):
-		if subj_date_list[i] in subj_visit:
+		if subj_date_list[i] not in subj_visit:
 			subj_visit[subj_date_list[i]] = 1
 		else:
 			subj_visit[subj_date_list[i]] += 1
@@ -64,5 +70,5 @@ for i in zip_list:
 		var3 = zip_path + "/" + i
 		var4 = modality
 		var5 = modality_type
-		cmd = "qsubcmd bash ./level00_man-subject.sh " + var1 + " " + var2 + " " + var3 + " " + var4 + " " + var5
+		cmd = "qsubcmd bash ./level00_man_rename-subject.sh " + var1 + " " + var2 + " " + var3 + " " + var4 + " " + var5
 		os.system(cmd)
