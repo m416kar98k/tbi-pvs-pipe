24a2125f07bf07bf8011398c56588836c564089e
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-04-01 16:09:34 -0700

    Update level01-preproc-standardize.sh

--- level01-preproc-standardize.sh
+++ level01-preproc-standardize.sh
@@ -11,20 +11,19 @@
 
 # recall var1 is the analysis absolute path
 cd ${1}
-for i in *; do 
-	if [ ! -f $i/smri/t1w.orig.nii.gz ]; then
-    		# backup the scans
-		qsubcmd mv ${i}/smri/t1w.nii.gz ${i}/smri/t1w.orig.nii.gz
-    		qsubcmd mv ${i}/smri/t2w.nii.gz ${i}/smri/t2w.orig.nii.gz
-		qsubcmd mv ${i}/smri/flair.nii.gz ${i}/smri/flair.orig.nii.gz
-		qsubcmd mv ${i}/dmri/dwi.bvec ${i}/dmri/dwi.orig.bvec
-		qsubcmd mv ${i}/dmri/dwi.nii.gz ${i}/dmri/dwi.orig.nii.gz
+for i in *
+do
+	# backup the scans
+	qsubcmd mv ${i}/smri/t1w.nii.gz ${i}/smri/t1w.orig.nii.gz
+    	qsubcmd mv ${i}/smri/t2w.nii.gz ${i}/smri/t2w.orig.nii.gz
+	qsubcmd mv ${i}/smri/flair.nii.gz ${i}/smri/flair.orig.nii.gz
+	qsubcmd mv ${i}/dmri/dwi.bvec ${i}/dmri/dwi.orig.bvec
+	qsubcmd mv ${i}/dmri/dwi.nii.gz ${i}/dmri/dwi.orig.nii.gz
 		
-    		# standardize the scans
-		qsubcmd qit VolumeStandardize --input ${i}/smri/t1w.orig.nii.gz --output ${i}/smri/t1w.nii.gz
-    		qsubcmd qit VolumeStandardize --input ${i}/smri/t2w.orig.nii.gz --output ${i}/smri/t2w.nii.gz
-		qsubcmd qit VolumeStandardize --input ${i}/smri/flair.orig.nii.gz --output ${i}/smri/flair.nii.gz
-		qsubcmd qit VolumeStandardize --input ${i}/dmri/dwi.orig.nii.gz --output ${i}/dmri/dwi.nii.gz --xfm ${i}/dmri/nii/dwi.xfm.txt
-		qsubcmd qit GradientsTransform --flip x --input ${i}/dmri/nii/dwi.orig.bvec --output ${i}/dmri/input/dwi.bvec
-	fi
+    	# standardize the scans
+	qsubcmd qit VolumeStandardize --input ${i}/smri/t1w.orig.nii.gz --output ${i}/smri/t1w.nii.gz
+    	qsubcmd qit VolumeStandardize --input ${i}/smri/t2w.orig.nii.gz --output ${i}/smri/t2w.nii.gz
+	qsubcmd qit VolumeStandardize --input ${i}/smri/flair.orig.nii.gz --output ${i}/smri/flair.nii.gz
+	qsubcmd qit VolumeStandardize --input ${i}/dmri/dwi.orig.nii.gz --output ${i}/dmri/dwi.nii.gz --xfm ${i}/dmri/dwi.xfm.txt
+	qsubcmd qit GradientsTransform --flip x --input ${i}/dmri/nii/dwi.orig.bvec --output ${i}/dmri/input/dwi.bvec
 done
