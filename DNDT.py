d92429f38a2e6c98c3ca9ec77cde5e2cd0e0b8e4
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-04-24 14:34:17 -0700

    Update DNDT.py

--- DNDT.py
+++ DNDT.py
@@ -1,13 +1,9 @@
-#!/usr/local/fsl/fslpython/bin/python
-
 # https://arxiv.org/pdf/1806.06988.pdf
-# my name is luka livin on a prayer
 
 from jax._src.api import value_and_grad
 from jax._src.numpy import lax_numpy as np
 from jax._src.nn import functions as nn
 from jax._src.random import PRNGKey, uniform
-# from jax._src.tree_util import tree_map, tree_reduce as map, reduce
 from jax.experimental.optimizers import adam
 from functools import reduce
 
@@ -22,6 +18,14 @@ def nn_decision_tree_cross_entropy(inputs, targets, cut_points_list, leaf_score,
     leaf = reduce(np.kron, map(lambda z: jax_soft_binning(inputs[:, z[0]:z[0] + 1], z[1], temperature), enumerate(cut_points_list)))
     preds = np.matmul(leaf, leaf_score)
     return -np.mean(targets * np.log(np.clip(preds, epsilon, 1. - epsilon)))
+
+''' experimental
+from jax._src.tree_util import tree_map, tree_reduce as map, reduce
+def nn_decision_tree_cross_entropy(inputs, targets, cut_points_list, leaf_score, temperature = 0.1, epsilon = 1e-12):
+    leaf = reduce(np.kron, map(lambda z: jax_soft_binning(np.reshape(inputs[:, z], [-1, 1]), cut_points_list[z], temperature), [*range(feature_dim)]))
+    preds = np.matmul(leaf, leaf_score)
+    return -np.mean(targets * np.log(np.clip(preds, epsilon, 1. - epsilon)))
+'''
 
 # set seed
 key = PRNGKey(0)
@@ -179,20 +183,22 @@ iris = [((5.1, 3.5, 1.4, 0.2), (1, 0, 0)),
         ((5.9, 3.0, 5.1, 1.8), (0, 0, 1))]
 feature = np.vstack([np.array(i[0]) for i in iris])
 label = np.vstack([np.array(i[1]) for i in iris])
+label = np.argmax(label, axis = 1)
 feature_dim = feature.shape[1]
 label_dim = label.shape[1]
-label = np.argmax(label, axis = 1)
-
-# set params
 cut_points_list = [uniform(key, [1]) for i in range(feature_dim)]
 leaf_score = uniform(key, [2 ** feature_dim, label_dim])
+
+# set params
+batch_size = 8
+num_steps = 10
 step_size = 1e-3
 opt_init, opt_update, get_params = adam(step_size)
 opt_state = opt_init(cut_points_list + [leaf_score])
-num_epochs = 10
 
-for i in range(num_epochs):
-    loss, grad = value_and_grad(nn_decision_tree_cross_entropy)(feature, label, cut_points_list, leaf_score)
+for i in range(num_steps):
+    batch = randint(key, (1, batch_size), 0, len(iris) - 1)
+    batch_feature, batch_label = feature[batch], label[batch]
+    loss, grads = value_and_grad(nn_decision_tree_cross_entropy)(batch_feature, batch_label, cut_points_list, leaf_score)
     opt_state = opt_update(i, grads, opt_state)
     cut_points_list, leaf_score = get_params(opt_state)
-    print("epoch " + str(i) + " loss " + str(loss))
