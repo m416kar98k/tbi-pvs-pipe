6fbfe0edc2c81a76cb9c057cae1f06593fa0b387
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-08-05 22:59:10 -0500

    Update and rename level03-qitout2table.py to level03-qitdiff2table.py

--- level03-qitdiff2table.py
+++ level03-qitdiff2table.py
@@ -8,7 +8,7 @@ for subject in sorted(os.listdir(sys.argv[1])):
   print("loading "+subject+"/"+visit)
   column_names=[]
   column_values=[subject,visit]
-  for modality in ["dkgm","dkwm","scgm"]:
+  for modality in ["ccwm","dkgm","dkwm","scgm"]:
     csv_path=sys.argv[1]+"/"+subject+"/qitout/"+visit+"/tone.region/fs."+modality+".dti.map"
     if os.path.isdir(csv_path):
      for j in sorted(os.listdir(csv_path)):
@@ -16,4 +16,4 @@ for subject in sorted(os.listdir(sys.argv[1])):
       column_names+=[modality+"_"+j.replace(".csv", "")+"_"+k for k in temp["name"]]
       column_values+=list(temp["value"])
       df2.append(column_values)
-pd.DataFrame(df2,columns=["Subject","Visit"]+column_names).to_csv(sys.argv[2]+"/qitout.all.csv",index=False)
+pd.DataFrame(df2,columns=["Subject","Visit"]+column_names).to_csv(sys.argv[2],index=False)
