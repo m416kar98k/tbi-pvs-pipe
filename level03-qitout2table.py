1f29cec89d5ccd5494bb1eddd0a059cb83e2b284
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-08-05 19:17:14 -0500

    Update level03-qitout2table.py

--- level03-qitout2table.py
+++ level03-qitout2table.py
@@ -3,15 +3,15 @@ import sys
 import os
 import pandas as pd
 df2 = []
-for subject in os.listdir(sys.argv[1]):
- for visit in os.listdir(sys.argv[1]+"/"+subject+"/qitout"):
+for subject in sorted(os.listdir(sys.argv[1])):
+ for visit in sorted(os.listdir(sys.argv[1]+"/"+subject+"/qitout")):
   print("loading "+subject+"/"+visit)
   for modality in ["dkgm","dkwm","scgm"]:
     csv_path=sys.argv[1]+"/"+subject+"/qitout/"+visit+"/tone.region/fs."+modality+".dti.map"
     if os.path.isdir(csv_path):
      column_names=[]
      column_values=[subject,visit]
-     for j in os.listdir(csv_path):
+     for j in sorted(os.listdir(csv_path)):
       temp=pd.read_csv(csv_path+"/"+j)
       column_names=[modality+"_"+j.replace(".csv", "")+"_"+k for k in temp["name"]]
       column_values+=list(temp["value"])
