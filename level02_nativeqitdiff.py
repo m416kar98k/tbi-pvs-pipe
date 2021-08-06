0ced55e3688bc8274e94e7716b7900ef02032c46
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-08-06 00:37:23 -0500

    Update level02_nativeqitdiff.py

--- level02_nativeqitdiff.py
+++ level02_nativeqitdiff.py
@@ -3,4 +3,4 @@ import sys
 import os
 for subject in sorted(os.listdir(sys.argv[1])):
  for visit in sorted(os.listdir(sys.argv[1]+"/"+subject+"/raw")):
-  os.system("qsubcmd qitdiff --motion --subject "+sys.argv[1]+"/"+subject+"/qitout --dwi "+sys.argv[1]+"/"+subject+"/raw/"+visit+"/dmri/dwi.nii.gz --bvecs "+sys.argv[1]+"/"+subject+"/raw/"+visit+"/dmri/dwi.bvec --bvals "+sys.argv[1]+"/"+subject+"/raw/"+visit+"/dmri/dwi.bval tone.region/fs.ccwm.dti.map tone.region/fs.scgm.dti.map tone.region/fs.dkwm.dti.map tone.region/fs.dkgm.dti.map")
+  os.system("qsubcmd qitdiff --motion --subject "+sys.argv[1]+"/"+subject+"/qitout --freesurfer "+sys.argv[1]+"/"+subject+"/fsout/"+visit+".long."+subject+" --dwi "+sys.argv[1]+"/"+subject+"/raw/"+visit+"/dmri/dwi.nii.gz --bvecs "+sys.argv[1]+"/"+subject+"/raw/"+visit+"/dmri/dwi.bvec --bvals "+sys.argv[1]+"/"+subject+"/raw/"+visit+"/dmri/dwi.bval tone.region/fs.ccwm.dti.map tone.region/fs.scgm.dti.map tone.region/fs.dkwm.dti.map tone.region/fs.dkgm.dti.map")
